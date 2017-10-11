dab740212eed8b7b468c4ae51ba13c15d517a387
Salman <salmansantino@gmail.com>
2017-10-11 21:08:17 +0600

    Title Bar Fixed 2

--- app/src/main/java/com/example/salman/calculation/MainActivity.java
+++ app/src/main/java/com/example/salman/calculation/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.res.ColorStateList;
 import android.media.Image;
 import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.titile);
+
         init();
 
         btn0.setOnClickListener(numclicks);
